890357fa2ef6a048727c7db5a8a811f1790cef8d
Lorberm <markus.lorber@outlook.com>
2018-02-18 23:36:24 +0100

    Add files via upload

---
+++ Controlsystem/Controlsystem/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace Controlsystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_on_5a = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_off_5a = new System.Windows.Forms.Button();
+            this.btn_on_3a = new System.Windows.Forms.Button();
+            this.btn_off_3a = new System.Windows.Forms.Button();
+            this.btn_on_1a = new System.Windows.Forms.Button();
+            this.btn_off_1a = new System.Windows.Forms.Button();
+            this.btn_on_1b = new System.Windows.Forms.Button();
+            this.btn_off_1b = new System.Windows.Forms.Button();
+            this.btn_AD = new System.Windows.Forms.Button();
+            this.label_Control = new System.Windows.Forms.Label();
+            this.btn_mail = new System.Windows.Forms.Button();
+            this.label_curr = new System.Windows.Forms.Label();
+            this.btn_info = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.label_5a = new System.Windows.Forms.Label();
+            this.label_3a = new System.Windows.Forms.Label();
+            this.label_1a = new System.Windows.Forms.Label();
+            this.label_1b = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_on_5a
+            // 
+            this.btn_on_5a.Location = new System.Drawing.Point(31, 159);
+            this.btn_on_5a.Name = "btn_on_5a";
+            this.btn_on_5a.Size = new System.Drawing.Size(103, 31);
+            this.btn_on_5a.TabIndex = 0;
+            this.btn_on_5a.Text = "5a";
+            this.btn_on_5a.UseVisualStyleBackColor = true;
+            this.btn_on_5a.Click += new System.EventHandler(this.btn_on_5a_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(198, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(256, 45);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Controlsystem";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(39, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 36);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Internet on:";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(198, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 36);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Internet off:";
+            // 
+            // btn_off_5a
+            // 
+            this.btn_off_5a.Location = new System.Drawing.Point(203, 159);
+            this.btn_off_5a.Name = "btn_off_5a";
+            this.btn_off_5a.Size = new System.Drawing.Size(103, 31);
+            this.btn_off_5a.TabIndex = 5;
+            this.btn_off_5a.Text = "5a";
+            this.btn_off_5a.UseVisualStyleBackColor = true;
+            this.btn_off_5a.Click += new System.EventHandler(this.btn_off_5a_Click);
+            // 
+            // btn_on_3a
+            // 
+            this.btn_on_3a.Location = new System.Drawing.Point(31, 208);
+            this.btn_on_3a.Name = "btn_on_3a";
+            this.btn_on_3a.Size = new System.Drawing.Size(103, 31);
+            this.btn_on_3a.TabIndex = 6;
+            this.btn_on_3a.Text = "3a";
+            this.btn_on_3a.UseVisualStyleBackColor = true;
+            this.btn_on_3a.Click += new System.EventHandler(this.btn_on_3a_Click);
+            // 
+            // btn_off_3a
+            // 
+            this.btn_off_3a.Location = new System.Drawing.Point(203, 208);
+            this.btn_off_3a.Name = "btn_off_3a";
+            this.btn_off_3a.Size = new System.Drawing.Size(103, 31);
+            this.btn_off_3a.TabIndex = 6;
+            this.btn_off_3a.Text = "3a";
+            this.btn_off_3a.UseVisualStyleBackColor = true;
+            this.btn_off_3a.Click += new System.EventHandler(this.btn_off_3a_Click);
+            // 
+            // btn_on_1a
+            // 
+            this.btn_on_1a.Location = new System.Drawing.Point(31, 258);
+            this.btn_on_1a.Name = "btn_on_1a";
+            this.btn_on_1a.Size = new System.Drawing.Size(103, 31);
+            this.btn_on_1a.TabIndex = 6;
+            this.btn_on_1a.Text = "1a";
+            this.btn_on_1a.UseVisualStyleBackColor = true;
+            this.btn_on_1a.Click += new System.EventHandler(this.btn_on_1a_Click);
+            // 
+            // btn_off_1a
+            // 
+            this.btn_off_1a.Location = new System.Drawing.Point(203, 260);
+            this.btn_off_1a.Name = "btn_off_1a";
+            this.btn_off_1a.Size = new System.Drawing.Size(103, 31);
+            this.btn_off_1a.TabIndex = 6;
+            this.btn_off_1a.Text = "1a";
+            this.btn_off_1a.UseVisualStyleBackColor = true;
+            this.btn_off_1a.Click += new System.EventHandler(this.btn_off_1a_Click);
+            // 
+            // btn_on_1b
+            // 
+            this.btn_on_1b.Location = new System.Drawing.Point(31, 312);
+            this.btn_on_1b.Name = "btn_on_1b";
+            this.btn_on_1b.Size = new System.Drawing.Size(103, 31);
+            this.btn_on_1b.TabIndex = 6;
+            this.btn_on_1b.Text = "1b";
+            this.btn_on_1b.UseVisualStyleBackColor = true;
+            this.btn_on_1b.Click += new System.EventHandler(this.btn_on_1b_Click);
+            // 
+            // btn_off_1b
+            // 
+            this.btn_off_1b.Location = new System.Drawing.Point(203, 312);
+            this.btn_off_1b.Name = "btn_off_1b";
+            this.btn_off_1b.Size = new System.Drawing.Size(103, 31);
+            this.btn_off_1b.TabIndex = 6;
+            this.btn_off_1b.Text = "1b";
+            this.btn_off_1b.UseVisualStyleBackColor = true;
+            this.btn_off_1b.Click += new System.EventHandler(this.btn_off_1b_Click);
+            // 
+            // btn_AD
+            // 
+            this.btn_AD.Location = new System.Drawing.Point(426, 159);
+            this.btn_AD.Name = "btn_AD";
+            this.btn_AD.Size = new System.Drawing.Size(111, 39);
+            this.btn_AD.TabIndex = 7;
+            this.btn_AD.Text = "Import into AD";
+            this.btn_AD.UseVisualStyleBackColor = true;
+            this.btn_AD.Click += new System.EventHandler(this.btn_AD_Click);
+            // 
+            // label_Control
+            // 
+            this.label_Control.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_Control.ForeColor = System.Drawing.Color.Black;
+            this.label_Control.Location = new System.Drawing.Point(422, 120);
+            this.label_Control.Name = "label_Control";
+            this.label_Control.Size = new System.Drawing.Size(148, 36);
+            this.label_Control.TabIndex = 8;
+            this.label_Control.Text = "Other Controls";
+            // 
+            // btn_mail
+            // 
+            this.btn_mail.Location = new System.Drawing.Point(426, 208);
+            this.btn_mail.Name = "btn_mail";
+            this.btn_mail.Size = new System.Drawing.Size(111, 39);
+            this.btn_mail.TabIndex = 7;
+            this.btn_mail.Text = "Export Mail";
+            this.btn_mail.UseVisualStyleBackColor = true;
+            this.btn_mail.Click += new System.EventHandler(this.btn_mail_Click);
+            // 
+            // label_curr
+            // 
+            this.label_curr.AutoSize = true;
+            this.label_curr.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_curr.ForeColor = System.Drawing.Color.Green;
+            this.label_curr.Location = new System.Drawing.Point(28, 392);
+            this.label_curr.Name = "label_curr";
+            this.label_curr.Size = new System.Drawing.Size(141, 16);
+            this.label_curr.TabIndex = 9;
+            this.label_curr.Text = "Currently turned on:";
+            this.label_curr.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btn_info
+            // 
+            this.btn_info.Location = new System.Drawing.Point(10, 12);
+            this.btn_info.Name = "btn_info";
+            this.btn_info.Size = new System.Drawing.Size(47, 47);
+            this.btn_info.TabIndex = 10;
+            this.btn_info.Text = "Info";
+            this.btn_info.UseVisualStyleBackColor = true;
+            this.btn_info.Click += new System.EventHandler(this.btn_info_Click);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.BackColor = System.Drawing.Color.Red;
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.ForeColor = System.Drawing.Color.White;
+            this.btn_exit.Location = new System.Drawing.Point(544, 23);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(47, 47);
+            this.btn_exit.TabIndex = 11;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = false;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // label_5a
+            // 
+            this.label_5a.AutoSize = true;
+            this.label_5a.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_5a.Location = new System.Drawing.Point(28, 418);
+            this.label_5a.Name = "label_5a";
+            this.label_5a.Size = new System.Drawing.Size(25, 16);
+            this.label_5a.TabIndex = 12;
+            this.label_5a.Text = "5a";
+            this.label_5a.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // label_3a
+            // 
+            this.label_3a.AutoSize = true;
+            this.label_3a.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_3a.Location = new System.Drawing.Point(59, 418);
+            this.label_3a.Name = "label_3a";
+            this.label_3a.Size = new System.Drawing.Size(25, 16);
+            this.label_3a.TabIndex = 12;
+            this.label_3a.Text = "3a";
+            this.label_3a.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // label_1a
+            // 
+            this.label_1a.AutoSize = true;
+            this.label_1a.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_1a.Location = new System.Drawing.Point(90, 418);
+            this.label_1a.Name = "label_1a";
+            this.label_1a.Size = new System.Drawing.Size(25, 16);
+            this.label_1a.TabIndex = 12;
+            this.label_1a.Text = "1a";
+            this.label_1a.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // label_1b
+            // 
+            this.label_1b.AutoSize = true;
+            this.label_1b.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_1b.Location = new System.Drawing.Point(122, 418);
+            this.label_1b.Name = "label_1b";
+            this.label_1b.Size = new System.Drawing.Size(25, 16);
+            this.label_1b.TabIndex = 12;
+            this.label_1b.Text = "1b";
+            this.label_1b.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(614, 491);
+            this.Controls.Add(this.label_1b);
+            this.Controls.Add(this.label_1a);
+            this.Controls.Add(this.label_3a);
+            this.Controls.Add(this.label_5a);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_info);
+            this.Controls.Add(this.label_curr);
+            this.Controls.Add(this.label_Control);
+            this.Controls.Add(this.btn_mail);
+            this.Controls.Add(this.btn_AD);
+            this.Controls.Add(this.btn_off_3a);
+            this.Controls.Add(this.btn_off_1a);
+            this.Controls.Add(this.btn_off_1b);
+            this.Controls.Add(this.btn_on_1b);
+            this.Controls.Add(this.btn_on_1a);
+            this.Controls.Add(this.btn_on_3a);
+            this.Controls.Add(this.btn_off_5a);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_on_5a);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Name = "Form1";
+            this.Text = "Controlsystem_MLO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_on_5a;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_off_5a;
+        private System.Windows.Forms.Button btn_on_3a;
+        private System.Windows.Forms.Button btn_off_3a;
+        private System.Windows.Forms.Button btn_on_1a;
+        private System.Windows.Forms.Button btn_off_1a;
+        private System.Windows.Forms.Button btn_on_1b;
+        private System.Windows.Forms.Button btn_off_1b;
+        private System.Windows.Forms.Button btn_AD;
+        private System.Windows.Forms.Label label_Control;
+        private System.Windows.Forms.Button btn_mail;
+        private System.Windows.Forms.Label label_curr;
+        private System.Windows.Forms.Button btn_info;
+        private System.Windows.Forms.Button btn_exit;
+        private System.Windows.Forms.Label label_5a;
+        private System.Windows.Forms.Label label_3a;
+        private System.Windows.Forms.Label label_1a;
+        private System.Windows.Forms.Label label_1b;
+    }
+}
+
